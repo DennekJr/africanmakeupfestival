8afc2b6796da264de9e56ef705cdade9853401f4
DennekJr <ohirikennedy@yahoo.com>
2024-10-10 14:03:01 +0100

    Home page Sponsors/partnership segment

---
+++ src/app/(home)/components/Partnership/utils.tsx
@@ -0,0 +1,10 @@
+export const PartnershipText = () => {
+  return (
+    <span className="relative max-w-full break-words">
+      The Africa Startup Festival relies on strategic partnerships and
+      sponsorships to unite thousands of people yearly, offering unparalleled
+      brand exposure and lead generation for our partners. Join us and be part
+      of Africa&apos;s premier entrepreneurial event!
+    </span>
+  );
+};
