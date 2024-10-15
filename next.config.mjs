1f11b71a7dd253c9fca10357807168d9a28fb8e6
DennekJr <ohirikennedy@yahoo.com>
2024-10-15 10:43:57 +0100

    Deployment fix - 8

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['b0nl3vocaq4tkgso.public.blob.vercel-storage.com'],
+  },
+};
 
 export default nextConfig;
