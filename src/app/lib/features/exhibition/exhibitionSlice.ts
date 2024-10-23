9c74de0f5229897712f32a44767889b05a9b8f0a
DennekJr <ohirikennedy@yahoo.com>
2024-10-23 18:56:34 +0100

    Exhibition slice fix - 2

--- src/app/lib/features/exhibition/exhibitionSlice.ts
+++ src/app/lib/features/exhibition/exhibitionSlice.ts
@@ -43,6 +43,7 @@ export const exhibitionSlice = createSlice({
 
     setFormValues: (state, action) => {
       state.formValues = ({
+        ...state.formValues,
         [action.payload.booth]: action.payload.data,
       });
     },
