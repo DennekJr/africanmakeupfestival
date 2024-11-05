c7864dcaa637b313e5adc7a66814aa0fb41028bc
DennekJr <ohirikennedy@yahoo.com>
2024-11-05 18:45:01 +0100

    Memoized build fix - 2

--- src/app/(home)/exhibit/register/SubPages/AddToBooth/index.tsx
+++ src/app/(home)/exhibit/register/SubPages/AddToBooth/index.tsx
@@ -21,8 +21,7 @@ export const AddToBooth = () => {
   const memoizedWith = useMemo(() => width, [width]);
 
   useEffect(() => {
-    if(typeof memoizedWith === 'undefined') return;
-    if(memoizedWith > 768){
+    if(typeof memoizedWith === 'undefined' && memoizedWith > 768){
       setIsDisabled(true);
     } else {
       setIsDisabled(false);
