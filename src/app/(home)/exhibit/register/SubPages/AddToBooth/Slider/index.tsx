836de0a57fa5817dcf81f0c10aac8f8498c68685
DennekJr <ohirikennedy@yahoo.com>
2024-11-05 18:46:50 +0100

    Memoized build fix - 3

--- src/app/(home)/exhibit/register/SubPages/AddToBooth/Slider/index.tsx
+++ src/app/(home)/exhibit/register/SubPages/AddToBooth/Slider/index.tsx
@@ -40,7 +40,7 @@ export const BoothsSlider: React.FC<BoothSliderProps> = ({ swiperRef }) => {
     }
   };
   useEffect(() => {
-    if (memoizedWith < 768) {
+    if(typeof memoizedWith === 'undefined' && memoizedWith < 768){
       setShowSwiper(true);
     } else {
       setShowSwiper(false);
