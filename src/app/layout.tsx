ca184d415afbea0f6d1e879f6f547e34ac49bdf5
DennekJr <ohirikennedy@yahoo.com>
2024-11-05 13:12:52 +0100

    Re-added storeProvider.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import ThemeRegistry from "../app/components/ThemeRegistry/ThemeRegistry";
 import React from "react";
 import TopLoader from "../app/components/TopLoader";
 import METADATA_CONFIG from "../config/metadata";
+import StoreProvider from "@/app/storeProvider";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 export const metadata = METADATA_CONFIG;
@@ -14,14 +15,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-    <head>
-      <link rel="icon" href="/images/footerLogo.svg" sizes="128x128" type="image/svg+xml" className={'!bg-white'} />
-      <link rel="preconnect" href="https://fonts.googleapis.com/" />
-    </head>
-    <body className={`${inter.variable} font-sans`}>
-    <ThemeRegistry>
-    <TopLoader />
-          {children}
+      <head>
+        <link
+          rel="icon"
+          href="/images/footerLogo.svg"
+          sizes="128x128"
+          type="image/svg+xml"
+          className={"!bg-white"}
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com/" />
+      </head>
+      <body className={`${inter.variable} font-sans`}>
+        <ThemeRegistry>
+          <StoreProvider>
+            <TopLoader />
+            {children}
+          </StoreProvider>
         </ThemeRegistry>
       </body>
     </html>
