cff1c7bca75d48cc838db5b0eec91537dcfe56b1
DennekJr <ohirikennedy@yahoo.com>
2024-11-16 17:18:24 +0100

    Navbar bg on sm screen fix - 2

--- src/app/components/Wrapper/index.tsx
+++ src/app/components/Wrapper/index.tsx
@@ -4,15 +4,17 @@ import { ReactNode } from "react";
 import Box from "@mui/material/Box";
 import NavBar from "../NavBar";
 import { Footer } from "../Footer";
+import { usePathname } from "next/navigation";
 
 export default function WebWrapper({ children }: { children: ReactNode }) {
+  const path = usePathname();
   return (
     <Box
       sx={{ display: "flex" }}
       className={"w-screen min-h-dvh flex flex-col relative"}
     >
       <NavBar />
-      <Box component="main" className={'pt-[65px] lg:pt-0'}>{children}</Box>
+      <Box component="main" sx={{ paddingTop: path !== "/" ? "65px" : "0" }} className={"lg:pt-0"}>{children}</Box>
       <Footer />
     </Box>
   );
