ba055aad5dfa168850cf9c475fd99c6c8a5f6155
DennekJr <ohirikennedy@yahoo.com>
2024-11-17 10:26:12 +0100

    Missing ErrorStatus type fix - 3

--- src/app/ErrorPage/functions.ts
+++ src/app/ErrorPage/functions.ts
@@ -1,8 +1,8 @@
 import { accessDenied, bugFixing, pageNotFound } from './assets/svg';
 import { ErrorStatus, ErrorMessage } from './constants';
 
-export const getErrorDetails = (status: ErrorStatus): JSX.Element | object | any => {
-  const getValues = (icon: JSX.Element, message: ErrorMessage): JSX.Element | object | any => ({
+export const getErrorDetails = (status: ErrorStatus): { appIcon: JSX.Element; message: ErrorMessage } | JSX.Element => {
+  const getValues = (icon: JSX.Element, message: ErrorMessage): { appIcon: JSX.Element; message: ErrorMessage } => ({
     appIcon: icon,
     message,
   });
