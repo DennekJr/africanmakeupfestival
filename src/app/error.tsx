b07c9a6fc4d359f1f6c4f5e09466c3e3c91ead4c
DennekJr <ohirikennedy@yahoo.com>
2024-11-18 01:41:51 +0100

    Build fix - 3

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,25 +4,6 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { Box, Button, Typography } from "@mui/material";
-import { notFound } from "@/app/ErrorPage/assets/svg";
-import { Result } from "antd";
-
-
-export enum ErrorStatus {
-  NOT_FOUND = "Seems like you're lost ",
-  ERROR = "Page crashed",
-  UNAUTHORIZED = "Unauthorized",
-  UNAUTHENTICATED = "Oops! You're not signed in",
-  BOOKING_CLOSED = "We are currently closed",
-}
-
-export enum ErrorMessage {
-  NOT_FOUND = "Page not found",
-  ERROR = "Oops, an error occurred!",
-  UNAUTHORIZED = `You're not authorized to view this resource`,
-  UNAUTHENTICATED = "Please sign in to continue.",
-  BOOKING_CLOSED = "Follow us on instagram @braidsbytifeh to see updates on when we open again.",
-}
 
 export default function Error({ error, reset }: { error: Error; reset: () => void }) {
   const router = useRouter();
@@ -30,11 +11,6 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
   useEffect(() => {
     console.error("Error caught:", error);
   }, [error]);
-  const extraButtons = [
-    <Button variant="outlined" onClick={() => router.push("/")} key="home">
-      Home
-    </Button>
-  ];
 
   return (
     <Box sx={{ textAlign: "center", padding: "50px" }}>
@@ -54,8 +30,6 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
       >
         Go to Homepage
       </Button>
-      <Result className={styles["result-error"]} title={ErrorStatus.NOT_FOUND} subTitle={ErrorMessage.NOT_FOUND}
-              icon={notFound} extra={extraButtons} />
     </Box>
   );
 }
