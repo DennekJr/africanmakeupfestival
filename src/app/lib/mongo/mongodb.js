5f781569bfabc4694c6cc05dc5afd2539c627e56
DennekJr <ohirikennedy@yahoo.com>
2024-11-21 19:26:21 +0100

    Fix db connection - 3

--- src/app/lib/mongo/mongodb.js
+++ src/app/lib/mongo/mongodb.js
@@ -1,10 +1,10 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGO_URL;
+const uri = process.env.MONGODB_URI;
 let client;
 let clientPromise;
 
-if (!process.env.MONGO_URL) {
+if (!process.env.MONGODB_URI) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
