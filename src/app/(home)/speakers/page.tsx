daa4bd5bcb98572638d2100a52f3e362a8345fb5
DennekJr <54910517+DennekJr@users.noreply.github.com>
2024-11-25 13:53:38 +0100

    Update page.tsx

--- src/app/(home)/speakers/page.tsx
+++ src/app/(home)/speakers/page.tsx
@@ -2,7 +2,7 @@ import Box from "@mui/material/Box";
 import * as React from "react";
 import { Hero } from "./Hero";
 import { LearnFromLeaders } from "./LearnFromLeaders";
-import { SpeakerFullGrid } from "./SpeakerFullGrid";
+// import { SpeakerFullGrid } from "./SpeakerFullGrid";
 import { MarqueeSlide } from "../components/Marquee";
 
 const Speakers: () => Promise<React.JSX.Element> = async () => {
