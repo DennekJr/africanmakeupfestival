e761de9d92d9a4a93e9f01c6d48e55b53e0f6b17
DennekJr <ohirikennedy@yahoo.com>
2024-11-25 13:25:04 +0100

    Disable payments

--- src/app/(home)/exhibit/register/SubPages/AddToBooth/DiscountCheckout/index.tsx
+++ src/app/(home)/exhibit/register/SubPages/AddToBooth/DiscountCheckout/index.tsx
@@ -115,7 +115,7 @@ const DiscountCheckout = () => {
           <button
             className="inline-flex items-center justify-center gap-3 ease-in-out duration-500 whitespace-nowrap text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[#F4F4F5] text-[#18181B] hover:bg-[#F4F4F5]/80 h-14 px-6 py-4 rounded-full relative w-full"
             type="button"
-            disabled={false}
+            disabled={true}
             onClick={handlePayment}
           >
             <span className="text-center w-full h-full">CHECKOUT NOW</span>
