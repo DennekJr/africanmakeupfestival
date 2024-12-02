d2529df36fb9816152dcd965c89fa22e2247bc98
DennekJr <54910517+DennekJr@users.noreply.github.com>
2024-12-02 20:21:40 +0100

    Update HeroButtons.tsx

--- src/app/(home)/components/Hero/HeroButtons.tsx
+++ src/app/(home)/components/Hero/HeroButtons.tsx
@@ -9,7 +9,7 @@ export const HeroButtons = () => {
       "flex flex-col lg:flex-row gap-4 w-full items-center justify-center"
     }
   >
-    <WhiteTicketButton href={"https://tix.africa/discover/asf2024/checkout?step=tickets"} name={"GET A TICKET"} />
+    <WhiteTicketButton href={"/ticket"} name={"GET A TICKET"} />
     <TransparentArrowButton name={"SPONSOR"} to={"/sponsor"} />
   </Box>;
 };
