ccb7f7a81555c9433f663d696c7d33d3eeb292ca
DennekJr <ohirikennedy@yahoo.com>
2024-12-03 16:42:36 +0100

    Edit ticket page design and add Default value for country in drop down

--- src/app/api/stripeTickets/route.js
+++ src/app/api/stripeTickets/route.js
@@ -23,7 +23,6 @@ export async function POST(request) {
     const db = client.db("africaskincarefestival"); // Replace with your database name
     // Get the data from the request body
     const {ticketData, session} = await request.json();
-    console.log(ticketData);
     Object.values(ticketData.buyerForm).map(async (detail) => {
       const TicketName = Object.keys(ticketData.buyerForm)[0];
       const buyerTicketPurchaseDetails = {
@@ -31,9 +30,6 @@ export async function POST(request) {
         form_lastName: detail[0][1].value,
         form_email: detail[0][4].value,
         form_confirmEmail: detail[0][5].value,
-        // form_organisation: detail[0][6].value,
-        // form_organisationWebsite: detail[0][7].value,
-        // form_organisationRole: detail[0][8].value,
         Ticket: TicketName.toUpperCase(), // Default ticket type
         form_phoneNumber: detail[0][2].value,
         form_howDidYouHearAboutEvent: detail[0][6].value,
@@ -56,9 +52,6 @@ export async function POST(request) {
         form_lastName: forms.form_lastName,
         form_email: forms.form_email,
         form_confirmEmail: forms.form_confirmEmail,
-        form_organisation: forms.form_organisation,
-        form_organisationWebsite: forms.form_organisationWebsite,
-        form_organisationRole: forms.form_organisationRole,
         Ticket: forms.Ticket.split("-")[0].toUpperCase(), // Default ticket type
       }
       const BuyerTicketPurchase = {
