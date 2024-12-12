6e191990a8505ee39b6c23ef8ee93a59e51dc928
DennekJr <ohirikennedy@yahoo.com>
2024-12-12 16:05:48 +0100

    Build fix

--- src/app/(home)/checkout/components/CheckoutForm.tsx
+++ src/app/(home)/checkout/components/CheckoutForm.tsx
@@ -265,9 +265,7 @@ const CheckoutForm = () => {
             )}
             {validatedCode.length > 0 && (
               <button
-                onClick={async (e) => {
-                  await handleValidatedCodePayment(e);
-                }}
+                onClick={handleValidatedCodePayment}
                 type={"submit"}
                 disabled={disabled}
                 className="animation-hover inline-flex items-center justify-center gap-3 ease-in-out duration-500 whitespace-nowrap text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 !bg-[#0A090B] text-gray-100 hover:bg-[$0A090B]/90 h-14 px-6 py-4 rounded-full relative w-full"
